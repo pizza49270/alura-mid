86946e2f199426286412375fe5f8c2f862e05aa5
pizza49270 <134529338+pizza49270@users.noreply.github.com>
2023-10-11 12:38:31 -0300

    Add files via upload

---
+++ alura-mid-main/alura-mid-main/main.js
@@ -0,0 +1,6 @@
+function tocasomplom (){
+    document.querySelector ('#som_tecla_pom').play('');
+    document.querySelector ('. tecla_pom').onclik = tocasomplom()
+}
+
+const listadeteclas = document.querySelectorAll('.tecla');
--- alura-mid-main/main.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.tecla_pom');
-
-document.querySelector('#som_tecla_pom').play('');
