53c50f4bde355144499fc48c85d5600bc6dd492d
pizza49270 <134529338+pizza49270@users.noreply.github.com>
2023-10-18 12:32:16 -0300

    Add files via upload

---
+++ alura-mid-main/alura-mid-main/alura-mid-main/alura-mid-main/main.js
@@ -0,0 +1,20 @@
+function tocasomplom (){
+    document.querySelector ('#som_tecla_pom').play('');
+   
+}
+const listadeteclas = document.querySelectorAll('.tecla');
+{
+    for (contador = 0; contador < listadeteclas.length; contador++);
+    const instrumento = listadeteclas[contador];
+    const tecla = listadeteclas[contador];
+    const idaudio = `#som_${instrumento}`;
+    listadeteclas[contador].classList[1];
+
+    tecla.onclik = tocasomplom
+    tocasomplom (`#som_tecla_plom`);
+
+
+listadeteclas[1].onclik = tocasomplom;
+}
+
+
